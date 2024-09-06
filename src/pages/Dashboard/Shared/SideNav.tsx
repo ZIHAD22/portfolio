5787be63faa2ec5465b5d34f2838d153da383640
ZIHAD22 <mdzihad2244@gmail.com>
2024-09-07 00:00:38 +0600

    dashboard menu design done

--- src/pages/Dashboard/Shared/SideNav.tsx
+++ src/pages/Dashboard/Shared/SideNav.tsx
@@ -1,10 +1,26 @@
+"use client";
 import React from "react";
 import Link from "next/link";
 import { Bell, Home, ListPlus } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Badge } from "@/components/ui/badge";
+import { usePathname } from "next/navigation";
+
+const dashboardMenu = [
+  {
+    name: "Dashboard",
+    path: "/dashboard",
+  },
+  {
+    name: "Work",
+    path: "/works",
+    count: 6,
+  },
+];
 
 const SideNav = () => {
+  const pathname = usePathname();
+
   return (
     <div className="hidden border-r lg:block">
       <div className="flex h-full max-h-screen flex-col gap-2">
@@ -20,24 +36,43 @@ const SideNav = () => {
           </Button>
         </div>
         <div className="flex-1">
-          <nav className="grid items-start px-2 text-sm font-medium xl:px-4">
-            <Link
-              href="/dashboard"
-              className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-accent"
-            >
-              <Home className="h-4 w-4" />
-              Dashboard
-            </Link>
-            <Link
-              href="/dashboard/works"
-              className="flex items-center gap-3 rounded-lg px-3 bg-[#b1b1cf1a] py-2 text-muted-foreground transition-all hover:text-accent"
-            >
-              <ListPlus />
-              Works
-              <Badge className="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full">
-                6
-              </Badge>
-            </Link>
+          <nav className="grid items-start px-2 text-sm font-medium xl:px-4 gap-4 mt-4">
+            {dashboardMenu.map((menu, i) => {
+              if (menu.name === "Dashboard") {
+                return (
+                  <Link
+                    key={i}
+                    href="/dashboard"
+                    className={`flex items-center gap-3 rounded-lg px-3 bg-[#b1b1cf1a] py-2  transition-all hover:text-accent ${
+                      pathname === menu.path
+                        ? "text-accent"
+                        : "text-muted-foreground"
+                    } `}
+                  >
+                    <ListPlus />
+                    {menu.name}
+                  </Link>
+                );
+              } else if (menu.count) {
+                return (
+                  <Link
+                    key={i}
+                    href={`/dashboard${menu.path}`}
+                    className={`flex items-center gap-3 rounded-lg px-3 bg-[#b1b1cf1a] py-2  transition-all hover:text-accent ${
+                      pathname === "/dashboard" + menu.path
+                        ? "text-accent"
+                        : "text-muted-foreground"
+                    } `}
+                  >
+                    <ListPlus />
+                    {menu.name}
+                    <Badge className="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full">
+                      {menu.count}
+                    </Badge>
+                  </Link>
+                );
+              }
+            })}
           </nav>
         </div>
       </div>
